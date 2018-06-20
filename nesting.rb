9455a9f347aa10235e5cc7a43c2e703e60f80330
EmilyHenken <20henkee@corjesu.org>
2018-06-20 19:28:32 +0000

    automatically backed up by learn

--- nesting.rb
+++ nesting.rb
@@ -1,24 +1,24 @@
 cities = {
   :New_York => {
     :mayor => "Bill DeBlasio",
-    :population => "8337000",
+    population => "8337000",
     :website => "http://www.nyv.gov"
   },
   
   :los_angeles => {
     :mayor => "Eric Garcetti",
-    :population => "3884307",
+    population => "3884307",
     :website => "http://www.lacity.org"
   },
   
   :miami => { 
     :mayor => "Tomás Regalado",
-    :population => 419777,
+    population => 419777,
     :website => "http://www.miamigov.com"
   },
   :chicago => { 
     :mayor => "Rahm Emanuel",
-    :population => 2695598,
+    population => 2695598,
     :website => "http://www.cityofchicago.org/"
   }
 }
