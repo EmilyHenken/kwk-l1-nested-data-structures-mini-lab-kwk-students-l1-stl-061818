ad7560595a78be7bbbe68e1cc3be049f4ce98e15
EmilyHenken <20henkee@corjesu.org>
2018-06-20 19:17:58 +0000

    automatically backed up by learn

---
+++ nesting.rb
@@ -0,0 +1,4 @@
+cities = {
+  :New_York => {:mayor => "Bill DeBlasio", :population => "8337000", :website => "http://www.nyv.gov"}
+  :LA => :mayor => "Eric Garcetti", :population = "3884307", :website => "http://www.lacity.org"
+}
