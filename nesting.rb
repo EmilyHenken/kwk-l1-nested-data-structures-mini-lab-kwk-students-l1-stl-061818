80fbc12b467d874daa168fd897237a21efafc733
EmilyHenken <20henkee@corjesu.org>
2018-06-20 19:28:48 +0000

    automatically backed up by learn

--- nesting.rb
+++ nesting.rb
@@ -24,8 +24,8 @@ cities = {
 }
 
 
-cities.each do |city, (population)|
+cities.each do |city, population|
   puts "The population of #{city} is #{population}."
 
 end
-  puts "The population of #{city} is #{(population)}."
+  puts "The population of #{city} is #{population}."
